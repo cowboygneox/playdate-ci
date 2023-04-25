c50d8f05831702c840599f8e7029b75f8bd5ec13
Sean Freitag <freitag.sean@gmail.com>
2023-04-25 08:57:20 -0500

    Fixing the Docker entrypoint.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-set -x
-
 export DISPLAY=:1
 
 Xvfb :1 -screen 0 1024x768x16 &
 
-cp -r /playdate/main.lua /tests/
-cp -r /playdate/luaunit /tests/
+cp -R /tests/* /runner/
+cp -r /playdate/main.lua /runner/
+cp -r /playdate/luaunit /runner/
 
 rm -fr ${PLAYDATE_SDK_PATH}/Disk/System/Launcher.pdx
 
-pdc -m /tests/main.lua ${PLAYDATE_SDK_PATH}/Disk/System/Launcher.pdx
+cd /runner
+pdc -m main.lua ${PLAYDATE_SDK_PATH}/Disk/System/Launcher.pdx
 
-PlaydateSimulator | tee playdate.log
+PlaydateSimulator | tee /runner/tests.log
 
-grep -E '^Ran ([0-9]+) tests in ([0-9]+\.[0-9]+) seconds, ([0-9]+) successes, 0 failures$' playdate.log
+grep -q -E '^Ran ([0-9]+) tests in ([0-9]+\.[0-9]+) seconds, ([0-9]+) successes, 0 failures$' /runner/tests.log
